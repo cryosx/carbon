87a202410c0c958077fd20d8a0c660aa664fadff
cryosx <cyruswu.email@gmail.com>
2015-11-19 08:09:52 -1000

    Removing .Idea
    You’ll need to add .gitignore into your .idea folder from now on

--- app/lib/collections/carbonstats.js
+++ app/lib/collections/carbonstats.js
@@ -1,5 +1,8 @@
 CarbonStats = new Mongo.Collection('carbonstats');
+<<<<<<< Updated upstream
 
 /*CarbonStats.before.insert(function(userId,doc){
     doc.createdAt = moment().toDate();
 });*/
+=======
+>>>>>>> Stashed changes
--- app/lib/collections/userrecords.js
+++ app/lib/collections/userrecords.js
@@ -2,7 +2,10 @@
  * Created by cryosx on 11/17/15.
  */
 UserRecords = new Mongo.Collection('userrecords');
+<<<<<<< Updated upstream
 
 /*UserRecords.before.insert(function(userId,doc){
     doc.createdAt = moment().toDate();
 });*/
+=======
+>>>>>>> Stashed changes
