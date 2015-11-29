124401ec9f1f9e5dd2aaf2efacf34d073db26a85
cryosx <cyruswu.email@gmail.com>
2015-11-29 01:40:04 -1000

    Fixed Carousel

--- app/client/templates/shared/carrousel/carousel.js
+++ app/client/templates/shared/carrousel/carousel.js
@@ -14,7 +14,7 @@ Template.carousel.onCreated(function () {
 Template.carousel.onRendered(function () {
     var owl = $(".owl-carousel").owlCarousel({
 
-        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        autoPlay: 5000, //Set AutoPlay to 3 seconds
         items : 4,
         itemsDesktop : [1199,3],
         itemsDesktopSmall : [979,3]
@@ -28,7 +28,7 @@ Template.carousel.onRendered(function () {
         owl.trigger('owl.prev');
     })
     $(".play").click(function(){
-        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+        owl.trigger('owl.play',5000); //owl.play event accept autoPlay speed as second parameter
     })
     $(".stop").click(function(){
         owl.trigger('owl.stop');
