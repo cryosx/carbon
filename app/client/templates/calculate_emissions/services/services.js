fe7a74cb0295d094d57324059f3a9783fdf70c37
cryosx <cyruswu.email@gmail.com>
2015-11-30 18:39:51 -1000

    DataBase Stuff Done, UI Problems Though

--- app/client/templates/calculate_emissions/services/services.js
+++ app/client/templates/calculate_emissions/services/services.js
@@ -10,10 +10,8 @@ Template.services.events({
         window.location.replace(url);
     },
     "submit": function() {
-        console.log("SUBMIT");
         event.preventDefault();
         if (validateTransport()) {
-            console.log("DONE VALIDATING");
             var record = {
                 userID: Meteor.userId(),
                 year: 2015,
@@ -66,18 +64,11 @@ Template.services.events({
             };
 
             var records = CarbonStats.find({userID: Meteor.userId(), year: 2015}).fetch();
-            console.log("CHECK LENGTH");
-            console.log(records.length);
-            console.log(records[0]._id);
-
             if (records.length === 1) {
                 CarbonStats.update({_id: records[0]._id}, record);
             } else if (records.length === 0) {
-                console.log("BEFORE INSET?");
                 CarbonStats.insert(record);
-                console.log("DEAD");
             }
-            console.log("AFTER");
         }
         return false;
     },
