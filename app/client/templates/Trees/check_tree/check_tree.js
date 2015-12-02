2efab83cd492e49168f85ebd2e49fcfd9df67a9a
cryosx <cyruswu.email@gmail.com>
2015-12-02 10:20:20 -1000

    Reorganization

---
+++ app/client/templates/Trees/check_tree/check_tree.js
@@ -0,0 +1,20 @@
+Template.checkTree.helpers({
+    //add you helpers here
+});
+
+Template.checkTree.events({
+    //add your events here
+});
+
+Template.checkTree.onCreated(function () {
+    //add your statement here
+});
+
+Template.checkTree.onRendered(function () {
+    //add your statement here
+});
+
+Template.checkTree.onDestroyed(function () {
+    //add your statement here
+});
+
--- app/client/templates/trees/trees.js
+++ app/client/templates/trees/trees.js
@@ -1,141 +1,18 @@
 Template.trees.helpers({
-
-    exampleMapOptions: function() {
-        // Make sure the maps API has loaded
-        if (GoogleMaps.loaded()) {
-            // Map initialization options
-            return {
-                center: new google.maps.LatLng(21.3500, -157.8000),
-                zoom: 12
-            };
-        }
-    },
-    specificFormData: function() {
-        return {
-            id: this._id,
-            other: this.other,
-            hard: 'Lolcats'
-        }
-    },
-    myCallbacks: function() {
-        return {
-            formData: function () {
-                return {id: "232323", other: Session.get("ReactiveParam")}
-            },
-            finished: function (index, fileInfo, context) {
-            },
-        }
-    }
 });
 
 Template.trees.events({
 
-    'click #save': function(event, template) {
-        console.log('Saving a tree');
-        var species = template.find("#species").value;
-        var location = template.find("#location").value;
-        var latitude = template.find("#latitude").value;
-        var longitude = template.find("#longitude").value;
-        var datePlanted = template.find("#datePlanted").value;
-        var diameter = template.find("#diameter").value;
-        //var createdDate = newDate();
-        var tree = {'userID':Meteor.user()._id,
-            'species':species,
-            'location':location,
-            'latitude':latitude,
-            'longitude':longitude,
-            'datePlanted':datePlanted,
-            'diameter':diameter,
-            //'created':createdDate
-        };
-        //tree['userID'] = Meteor.user()._id; //or tree.key = value
-        console.log(tree);
-        console.log(Meteor.user()._id);
-
-        TreeCollection.insert(tree);
-
-        var treeID;
-
-    },
-    "change #latitude, change #longitude": function() {
-        setMarker();
-    }
 });
 
-
-
-
 Template.trees.onCreated(function () {
-   init();
+
 });
 
 Template.trees.onRendered(function () {
-    $('select').material_select();
-    GoogleMaps.load();
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
+
 });
 
 Template.trees.onDestroyed(function () {
-    //add your statement here
+
 });
-function init() {
-    // We can use the `ready` callback to interact with the map API once the map is ready.
-    GoogleMaps.ready('exampleMap', function(map) {
-        // Add a marker to the map once it's ready
-        marker  =  new google.maps.Marker({
-            position: map.options.center,
-            map: map.instance,
-            draggable: true,
-            animation: google.maps.Animation.DROP
-        });
-        marker.addListener('click', toggleBounce);
-        marker.addListener('drag', updateLatLng);
-
-    });
-}
-
-
-function alertCoords(){
-    console.log("Marker: " + marker.getPosition().lat());
-}
-
-function setMarker() {
-    var lat  =  parseFloat($("#latitude").val());
-    var lng  =  parseFloat($("#longitude").val());
-    console.log("Lat: " + lat);
-    console.log("Lng: " + lng);
-
-    if (!isNaN(lat) || !isNaN(lng)) {
-        if (lat > -90 && lat < 90 && lng > -180 && lat < 90) {
-            console.log("good");
-        }
-    }
-    if (lat < 180 && lat > -180) {
-        //marker.setPosition({lat: lat, lng: lng})
-        //marker.getMap().setCenter({lat: lat, lng: lng});
-    }
-}
-
-function updateLatLng() {
-    $("#latitude").val(marker.getPosition().lat());
-    $("#longitude").val(marker.getPosition().lng());
-}
-
-function toggleBounce() {
-    if (marker.getAnimation() ! =  =  null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-}
-
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-}
